c68d11204ef97f0b92b1cdeb6e0ec9300c5a8c70
Harvey Kane <harvey@ragepank.com>
2012-04-01 08:49:10 +0000

    Freightmethods deletable.

--- install/autoupdate_cart_freightmethod.php
+++ install/autoupdate_cart_freightmethod.php
@@ -21,6 +21,7 @@ $default_td['cart_freightmethod'] = array(
         'td_rolloverfield' => "longname",
         'td_orderbyfields' => "displayorder, shortname",
         'td_menutype' => "list",
+        'td_deleteoption' => "yes",
         'td_defaultpermissions' => "everyone.show=0\neveryone.view=0\neveryone.edit=0\neveryone.add=0\neveryone.delete=0\nadmin.show=0\nadmin.view=0\nadmin.edit=0\nadmin.add=0\nadmin.delete=0\nnotloggedin.show=0\nnotloggedin.view=0\nnotloggedin.edit=0\nnotloggedin.add=0\nnotloggedin.delete=0\nregistered.show=0\nregistered.view=0\nregistered.edit=0\nregistered.add=0\nregistered.delete=0\nsysinstall.show=0\nsysinstall.view=0\nsysinstall.edit=0\nsysinstall.add=0\nsysinstall.delete=0\n",
     );
 
