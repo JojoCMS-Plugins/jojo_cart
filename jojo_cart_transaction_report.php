8c361ceabf694bed9cffdb91683425f4619440a1
antonyspalding <antony@spalding.gen.nz>
2013-07-17 12:25:33 +1200

    Adding link to download transactions as csv file

--- jojo_cart_transaction_report.php
+++ jojo_cart_transaction_report.php
@@ -28,7 +28,11 @@ class jojo_plugin_jojo_cart_transaction_report extends JOJO_Plugin
             echo $token;
             exit;
         }
-
+		
+		if (isset($_POST['submit'])) {
+            $sent = $this->exportLogs();
+        }
+		
         jojo_plugin_Admin::adminMenu();
 
         $transactions = Jojo::selectQuery("SELECT * FROM {cart} WHERE id > 0 ORDER BY id DESC LIMIT ".Jojo::getOption('cart_transactions_report_number', 150));
@@ -62,4 +66,78 @@ class jojo_plugin_jojo_cart_transaction_report extends JOJO_Plugin
 
         return $content;
     }
+
+	function exportLogs()
+    {
+        /* Check for form injection attempts */
+        Jojo::noFormInjection();
+        if (isset($_POST['filedownload'])) {
+        	
+			$transactions = Jojo::selectQuery("SELECT * FROM {cart} WHERE id > 0 ORDER BY id DESC ");
+			if (!count($transactions)) { return true; }
+
+			foreach ($transactions as $k => $s) {
+                //add stuff here to total quantities and products etc 
+                //$transactions[$k]['submitted'] = strftime('%F %T', $s['submitted']);
+                $transactions[$k]['date'] = strftime('%F %T', $s['updated']);
+                $cart = unserialize($transactions[$k]['data']);
+				$transactions[$k]['data'] = $cart;
+				
+				$items = $cart->items;
+				foreach($items as $itemkey => $itemvalue){
+					$transactions[$k]['items'] = $transaction[$k]['items'] . $itemvalue['quantity'] . " " . $itemvalue['name'] . " : ";
+				}
+				
+				foreach ($cart->fields as $ck => $cv) {
+					if (strpos($ck, 'shipping')!==false) {
+	                   $shipping[$ck] = $cv;
+					   foreach($shipping as $sk => $sv){
+					   		$transactions[$k][$sk] = trim(str_replace(array("\n", "\r"), ' ', $sv));
+					   }
+	                } elseif (strpos($ck, 'billing')!==false) {
+	                   $billing[$k] = $cv;
+					   foreach($billing as $sk => $sv){
+					   		$transactions[$k][$sk] = trim(str_replace(array("\n", "\r"), ' ', $sv));
+					   }
+	                }
+				}
+				
+				unset($transactions[$k]['updated']);
+				unset($transactions[$k]['data']);
+				unset($transactions[$k]['data_blob']);
+				unset($transactions[$k]['submitted']);
+				unset($transactions[$k]['ip']);
+				unset($transactions[$k]['userid']);
+				unset($transactions[$k]['token']);
+				unset($transactions[$k]['actioncode']);
+				unset($transactions[$k]['testmode']);
+            }
+			
+			$output = '';
+            $c=0;
+            foreach($transactions AS $array) {
+                $val_array = array();
+                $key_array = array();
+                foreach($array AS $key => $val) {
+                    $key_array[] = $key;
+                    $val = str_replace('"', '""', $val);
+                    $val_array[] = "\"$val\"";
+                }
+                if($c == 0) {
+                    $output .= implode(",", $key_array)."\n";
+                }
+                $output .= implode(",", $val_array)."\n";
+                $c++;
+            }
+			
+            header('Content-type: text/csv'."\r\n");
+            header('Content-disposition: attachment; filename="Transactions.csv"'."\r\n");
+            header("Pragma: no-cache"."\r\n");
+            header("Expires: 0"."\r\n");
+            header("Content-length: ".strlen($output)."\r\n");
+            echo $output;
+            exit;
+        }
+    }
+
 }
